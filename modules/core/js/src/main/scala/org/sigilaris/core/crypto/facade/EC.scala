28271e25867b88a45048f98d3c9a8cc070f081a1
sungkmi <sungkmi@gmail.com>
2025-10-22 23:16:44 +0900

    core(js): switch elliptic facade to namespace import; construct EC via dynamic new for ESM/CJS interop

--- modules/core/js/src/main/scala/org/sigilaris/core/crypto/facade/EC.scala
+++ modules/core/js/src/main/scala/org/sigilaris/core/crypto/facade/EC.scala
@@ -5,11 +5,9 @@ package facade
 import scala.scalajs.js
 import scala.scalajs.js.annotation.*
 import scala.scalajs.js.typedarray.Uint8Array
-import scala.annotation.unused
 
 @js.native
-@JSImport("elliptic", "ec")
-class EC(@unused curve: String) extends js.Object:
+trait EC extends js.Object:
   def genKeyPair(): JsKeyPair                     = js.native
   def keyFromPrivate(priv: Uint8Array): JsKeyPair = js.native
   def recoverPubKey(
@@ -19,7 +17,14 @@ class EC(@unused curve: String) extends js.Object:
       enc: js.UndefOr[String],
   ): BasePoint = js.native
 
+@js.native
+@JSImport("elliptic", JSImport.Namespace)
+object Elliptic extends js.Object:
+  val ec: js.Dynamic = js.native
+
 object EC:
-  def byName(name: String): EC = new EC(name)
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+  def byName(name: String): EC =
+    js.Dynamic.newInstance(Elliptic.ec)(name).asInstanceOf[EC]
 
   def secp256k1: EC = byName("secp256k1")
