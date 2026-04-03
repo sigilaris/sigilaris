6d53ad3f662898e5652ff651be58a47538d118f4
sungkmi <sungkmi@gmail.com>
2026-04-03 13:41:44 +0900

    Add bignatOrdering property tests and unify generator usage
    Test reflexive, antisymmetric, and transitive properties of
    BigNat.bignatOrdering. Unify all properties to use bignatGen.forAll
    consistently instead of mixing Gen alias styles.

--- modules/core/shared/src/test/scala/org/sigilaris/core/datatype/BigNatTest.scala
+++ modules/core/shared/src/test/scala/org/sigilaris/core/datatype/BigNatTest.scala
@@ -11,14 +11,13 @@ import hedgehog.*
 
 class BigNatTest extends HedgehogSuite:
 
-  val genBignat = Gen
+  private val bignatGen = Gen
     .bytes(Range.linear(0, 64))
     .map(BigInt(1, _))
     .map(BigNat.unsafeFromBigInt)
-    .forAll
 
   property("roundtrip of bignat byte codec"):
-    genBignat.map: bignat =>
+    bignatGen.forAll.map: bignat =>
       val encoded = bignat.toBytes
 
       encoded.to[BigNat] match
@@ -26,9 +25,37 @@ class BigNatTest extends HedgehogSuite:
         case _              => Result.failure
 
   property("roundtrip of bignat json codec"):
-    genBignat.map: bignat =>
+    bignatGen.forAll.map: bignat =>
       val encoded = bignat.toJson
 
       JsonDecoder[BigNat].decode(encoded) match
         case Right(decoded) => decoded ==== bignat
         case _              => Result.failure
+
+  property("bignat ordering is reflexive"):
+    bignatGen.forAll.map: bignat =>
+      BigNat.bignatOrdering.compare(bignat, bignat) ==== 0
+
+  property("bignat ordering is antisymmetric"):
+    for
+      x <- bignatGen.forAll
+      y <- bignatGen.forAll
+    yield
+      val forward = Integer.signum(BigNat.bignatOrdering.compare(x, y))
+      val reverse = Integer.signum(BigNat.bignatOrdering.compare(y, x))
+      forward ==== -reverse
+
+  property("bignat ordering is transitive"):
+    for
+      x <- bignatGen.forAll
+      y <- bignatGen.forAll
+      z <- bignatGen.forAll
+    yield
+      val xy = BigNat.bignatOrdering.compare(x, y)
+      val yz = BigNat.bignatOrdering.compare(y, z)
+      val xz = BigNat.bignatOrdering.compare(x, z)
+
+      Result.all(List(
+        if xy <= 0 && yz <= 0 then Result.assert(xz <= 0) else Result.success,
+        if xy >= 0 && yz >= 0 then Result.assert(xz >= 0) else Result.success,
+      ))
